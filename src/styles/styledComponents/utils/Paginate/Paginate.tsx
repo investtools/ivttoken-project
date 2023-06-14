20e9e88396f41f6db176f74d6df23e4c757f4484
Murillo Lamberti <lambertixx@gmail.com>
2023-06-13 22:56:59 -0300

    wrap listbox options

--- src/styles/styledComponents/utils/Paginate/Paginate.tsx
+++ src/styles/styledComponents/utils/Paginate/Paginate.tsx
@@ -19,7 +19,7 @@ const Paginate: React.FC<PaginateProps> = ({ totalPage, itemsPerPage, currentPag
     const handleItemsPerPageChange = (itemsPerPage: number) => {
         setItemsPerPage(itemsPerPage)
         setCurrentPage(1)
-      }
+    }
 
 
 
@@ -39,42 +39,44 @@ const Paginate: React.FC<PaginateProps> = ({ totalPage, itemsPerPage, currentPag
 
             <span className="text-ivtcolor2 font-bold">{"-"} {t.t("Page")} {currentPage} {t.t("of")} {totalPage} {"-"}</span>
 
-            <Listbox value={itemsPerPage} onChange={handleItemsPerPageChange}>
-                <Listbox.Button className="rounded-full p-2 text-ivtcolor2 font-bold">
-                    <div className="flex justify-between">
-                        <span>{itemsPerPage} {t.t("items per page")}</span>
-                        <ChevronUpDownIcon className="w-5 h-5 text-gray-400 mt-[3px]" aria-hidden="true" />
-                    </div>
-                </Listbox.Button>
-                <Listbox.Options className="border rounded absolute z-10 mt-1 bg-white shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 overflow-auto">
-                    {[5, 10, 20, 50].map((value) => (
-                        <Listbox.Option
-                            key={value}
-                            value={value}
-                            className={({ active }) =>
-                                `${active ? 'text-white bg-ivtcolor' : 'text-ivtcolor2'} 
+            <div>
+                <Listbox value={itemsPerPage} onChange={handleItemsPerPageChange}>
+                    <Listbox.Button className="rounded-full p-2 text-ivtcolor2 font-bold">
+                        <div className="flex justify-between">
+                            <span>{itemsPerPage} {t.t("items per page")}</span>
+                            <ChevronUpDownIcon className="w-5 h-5 text-gray-400 mt-[3px]" aria-hidden="true" />
+                        </div>
+                    </Listbox.Button>
+                    <Listbox.Options className="border rounded absolute z-10 mt-1 bg-white shadow-lg max-h-60 ring-1 ring-black ring-opacity-5 overflow-auto">
+                        {[5, 10, 20, 50].map((value) => (
+                            <Listbox.Option
+                                key={value}
+                                value={value}
+                                className={({ active }) =>
+                                    `${active ? 'text-white bg-ivtcolor' : 'text-ivtcolor2'} 
                     cursor-default select-none relative py-2 pl-10 pr-4 `}
-                        >
-                            {({ selected, active }) => (
-                                <>
-                                    <span
-                                        className={`${selected ? 'font-bold' : 'font-normal'} block truncate`}>
-                                        {value} {t.t("items per page")}
-                                    </span>
-                                    {selected ? (
+                            >
+                                {({ selected, active }) => (
+                                    <>
                                         <span
-                                            className={`${active ? 'text-white' : 'text-ivtcolor'
-                                                } absolute inset-y-0 left-0 flex items-center pl-3`}
-                                        >
-                                            <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                                            className={`${selected ? 'font-bold' : 'font-normal'} block truncate`}>
+                                            {value} {t.t("items per page")}
                                         </span>
-                                    ) : null}
-                                </>
-                            )}
-                        </Listbox.Option>
-                    ))}
-                </Listbox.Options>
-            </Listbox>
+                                        {selected ? (
+                                            <span
+                                                className={`${active ? 'text-white' : 'text-ivtcolor'
+                                                    } absolute inset-y-0 left-0 flex items-center pl-3`}
+                                            >
+                                                <CheckIcon className="w-5 h-5" aria-hidden="true" />
+                                            </span>
+                                        ) : null}
+                                    </>
+                                )}
+                            </Listbox.Option>
+                        ))}
+                    </Listbox.Options>
+                </Listbox>
+            </div>
 
             <button className="bg-ivtcolor hover:bg-hover text-white font-bold py-2 px-4 rounded-full" onClick={nextPage} disabled={currentPage === totalPage}>
                 {t.t("Next")}

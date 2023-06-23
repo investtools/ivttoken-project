e8f6cbfc69712e1c67f3e114f1cab55e5a87e90e
Murillo Lamberti <lambertixx@gmail.com>
2023-06-23 20:57:12 -0300

    fix cards

--- src/styles/styledComponents/loggedOutZone/institutional/Cards.tsx
+++ src/styles/styledComponents/loggedOutZone/institutional/Cards.tsx
@@ -31,7 +31,7 @@ const Cards: React.FC<CardsProps> = ({ path, name, description }) => {
       <div className="hover:scale-110 border-4 border-transparent hover:border-hover transition duration-500 grid card-shadow grid-cols-1 rounded p-6 flex flex-col justify-between items-start transform">
         <div>
           <div className="border-4 border-ivtcolor2">
-            <Image src={path} alt="team members pic" width={300} height={50} />
+            <Image src={path} alt="team members pic" width={800} height={50} />
           </div>
           <div className="grid mt-2 mb-2">
             <span className="text-ivtcolor2hover text-xl font-bold">{t.t(name)}</span>
--- src/styles/styledComponents/loggedOutZone/testimonials/Cards.tsx
+++ src/styles/styledComponents/loggedOutZone/testimonials/Cards.tsx
@@ -25,10 +25,10 @@ const Cards: React.FC<CardsProps> = ({ path, name, description }) => {
 
   return (
     <>
-      <div className="hover:scale-110 border-4 border-transparent hover:border-hover transition duration-500 grid card-shadow grid-cols-1 rounded p-6 flex flex-col justify-between items-start transform">
+      <div className="mb-4 hover:scale-110 border-4 border-transparent hover:border-hover transition duration-500 grid card-shadow grid-cols-1 rounded p-6 flex flex-col justify-between items-start transform">
         <div>
           <div className="border-4 border-ivtcolor2">
-            <Image src={path} alt="testimonials pic" width={300} height={50} />
+            <Image src={path} alt="testimonials pic" width={800} height={50} />
           </div>
           <div className="grid mt-4 mb-2">
             <span className="text-ivtcolor2hover text-xl font-bold mb-4">{t.t(name)}</span>

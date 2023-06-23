684c03aba01a93e3083699f4cae410b01de3091e
Murillo Lamberti <lambertixx@gmail.com>
2023-06-22 22:31:24 -0300

    fix enter button

--- src/styles/styledComponents/loggedOutZone/LoginButton.tsx
+++ src/styles/styledComponents/loggedOutZone/LoginButton.tsx
@@ -15,7 +15,7 @@ const LoginButton: React.FC = () => {
         <>
             <button
                 onClick={handleClick}
-                className="loginSvgEffect border border-white gradient-animation border-white shadow-sm text-sm font-medium text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-ivtcolor text-white font-bold py-2 px-4 rounded-full">
+                className="loginSvgEffect border border-white gradient-animation border-white shadow-sm text-sm font-medium text-white text-white font-bold py-2 px-4 rounded-full">
                 <span className="flex items-center font-bold">
                     {t.t("Enter")}&nbsp;&nbsp;
                     <div className="transform scale-x-[-1] ">

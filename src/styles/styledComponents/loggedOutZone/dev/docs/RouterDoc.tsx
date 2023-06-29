b0efeeb0d19d6bda0235872d716cd0c9600c7b83
Murillo Lamberti <lambertixx@gmail.com>
2023-06-29 15:59:37 -0300

    redesign router doc

--- src/styles/styledComponents/loggedOutZone/dev/docs/RouterDoc.tsx
+++ src/styles/styledComponents/loggedOutZone/dev/docs/RouterDoc.tsx
@@ -1,4 +1,6 @@
+import { useRouter } from 'next/router'
 import React from 'react'
+import { Translate } from 'translate/translate'
 
 interface RouteDocProps {
     input: string
@@ -10,15 +12,21 @@ interface RouteDocProps {
 }
 
 const RouterDocumentation: React.FC<RouteDocProps> = ({ title, description, requestType, input, output, procedure }) => {
+    const router = useRouter()
+    const locale = router.locale === undefined ? "en" : router.locale
+    const t = new Translate(locale)
+
     return (
         <div className="mb-4 hover:scale-110 border-4 border-transparent hover:border-hover transition duration-500 grid card-shadow grid-cols-1 rounded p-6 flex-col justify-between items-start transform">
-            <div className="mb-4">
-                <h3 className="text-xl mb-1">{title}:</h3>
-                <p className="mb-2">{description}</p>
-                {requestType === 'Mutation' ? <p className="mb-2">Input: {input}</p> : ""}
-                <p className="mb-2">Output: {output}</p>
-                <p className="mb-2">Procedure: {procedure}</p>
-                <p className="mb-2">Request Type: {requestType}</p>
+            <div>
+                <h3 className="text-xl mb-1 font-bold text-ivtcolor2">{title}:</h3>
+                <p className="mb-2 text-sm text-gray-500">{t.t(description)}</p>
+                <div className='grid grid-cols-2'>
+                    {requestType === 'Mutation' ? (<><p className="mb-2"><span className="border-b-2 text-ivtcolor2 font-bold border-ivtcolor2 inline-block">{t.t("Input")}:</span></p><p className='font-extrabold text-hover'>{input}</p></>) : ""}
+                    <p className="mb-2"><span className="border-b-2 text-ivtcolor2 font-bold border-ivtcolor2 inline-block">{t.t("Output")}:</span></p> <p className='font-extrabold text-hover'>{output}</p>
+                    <p className="mb-2"><span className="border-b-2 text-ivtcolor2 font-bold border-ivtcolor2 inline-block">{t.t("Procedure")}:</span></p> <p className='font-extrabold text-hover'>{t.t(procedure)}</p>
+                    <p className="mb-2"><span className="border-b-2 text-ivtcolor2 font-bold border-ivtcolor2 inline-block">{t.t("Request Type")}:</span></p> <p className='font-extrabold text-hover'>{requestType}</p>
+                </div>
             </div>
         </div>
     )

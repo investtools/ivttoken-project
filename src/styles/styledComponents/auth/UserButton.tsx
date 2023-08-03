e1d3e27f02a45a6b0dbac740cb5d74c0a3d06579
Murillo Lamberti <lambertixx@gmail.com>
2023-08-03 10:46:37 -0300

    fix contracts and add user email to header

--- src/styles/styledComponents/auth/UserButton.tsx
+++ src/styles/styledComponents/auth/UserButton.tsx
@@ -11,16 +11,19 @@ const UserButton = ({ locale }: { locale: string }) => {
     if (session && session.user) {
         return (
             <div className="flex">
+                <span className="flex items-center -translate-x-2 text-[14px] text-gray-400 p-1 bg-white rounded-full">{session.user.name}</span>
                 <div className="rounded-full cursor-pointer" onClick={() => setShowInfo(!showInfo)}>
-                    <Image width={35} height={1} src={session.user.image ?? ""} alt={'brazil'} className="rounded-full" />
-                </div>
-                {showInfo &&
-                    <div className="absolute translate-y-12 -translate-x-3 text-center">
-                        <button onClick={() => void signOut({ callbackUrl: '/main' })} className="border gradient-animation text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
-                            {t.t("Sign Out")}
-                        </button>
+                    <div>
+                        <Image width={35} height={1} src={session.user.image ?? ""} alt={'brazil'} className="rounded-full" />
                     </div>
-                }
+                    {showInfo &&
+                        <div className="absolute translate-y-3 -translate-x-3 text-center">
+                            <button onClick={() => void signOut({ callbackUrl: '/main' })} className="border gradient-animation text-white font-bold py-2 px-4 rounded-full focus:outline-none focus:shadow-outline">
+                                {t.t("Sign Out")}
+                            </button>
+                        </div>
+                    }
+                </div>
             </div>
         )
     }
